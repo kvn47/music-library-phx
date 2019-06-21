e2955666fc606aa70ee3be4fcb4c89a363dc7d4f
Vladimir Kochergin <kvn047@gmail.com>
2019-06-21 16:21:43 +1000

    deploy

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :music_library, MusicLibraryWeb.Endpoint,
-  server: true,
   url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: "./static"
 
 # Do not print debug messages in production
 config :logger, level: :info
